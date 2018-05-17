e2120ef9909f6057f03b3a8852b8261c525057b4
77edsilva <edmcostas@gmail.com>
2018-05-17 14:31:09 -0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/br/com/avana/tabajara/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/avana/tabajara/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package br.com.avana.tabajara;
+package br.com.cadastro.teste;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("br.com.avana.tabajara", appContext.getPackageName());
+        assertEquals("br.com.cadastro.teste", appContext.getPackageName());
     }
 }
